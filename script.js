af8f023c47f9f3d01cabb0427611ade226e05077
EtnoPolino <einshoch66@gmail.com>
2022-08-22 04:58:31 +0200

    AtW

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ const modal = document.querySelector('#modal');
 const addBookModal = document.querySelector('#modalBtn');
 const closeModalBtn = document.querySelector('.closeBtn');
 const submitBook = document.querySelector('button[type=submit]');
+const bookscontainer = document.querySelector('.bookcontainer');
+const inputTitle = document.querySelector('#title');
+const inputAuthor = document.querySelector('#author');
+const inputPages = document.querySelector('#pages');
+const checkbox = document.querySelector('input[type=checkbox]');
+
+/*const deleteNote = document.querySelector('.remove');
+const readOrNot = document.querySelector('.note > button:first-of-type'); */
 
 
 addBookModal.addEventListener('click', openModal);
 closeModalBtn.addEventListener('click', closeModal);
-/*submitBook.addEventListener('click', closeModal);*/
-window.addEventListener('click', clickOutside)
+submitBook.addEventListener('click', addBook);
+window.addEventListener('click', clickOutside);
+
+
+
 
 function openModal(){
     modal.style.display = 'block';
@@ -28,6 +39,45 @@ function clickOutside(e){
 
 
 
+function createBook(book){
+    const lastElem = book[book.length - 1]; /* get the last elem of the array */
+    
+    /*
+    const bookInfo = document.createElement('div');
+    const p_1 = document.createElement('p');
+    const p_2 = document.createElement('p');
+    const p_3 = document.createElement('p');
+    const deleteNote = document.createElement('button');
+    const readOrNot = document.createElement('button');
+   
+    
+
+    p_1.innerText = `Title : ${}`;
+    p_2.innerText = `Title : ${}`;
+    p_3.innerText = `Title : ${}`;  
+    deleteNote.innerText = `DELETE`;
+    readOrNot.innerText = `READ`;
+
+    bookInfo.classList.add('note');
+    deleteNote.classList.add('remove');
+
+    bookscontainer.appendChild(bookInfo);
+    bookInfo.appendChild(p_1);
+    bookInfo.appendChild(p_2);
+    bookInfo.appendChild(p_3);
+    bookInfo.appendChild(readOrNot);
+    bookInfo.appendChild(deleteNote);
+
+
+ 
+   closeModal(); */
+    log(lastElem);
+    log(lastElem.title);
+    log(lastElem.author);
+    log(lastElem.pages);
+    log(lastElem.isRead);
+} // end function createBook
+
 
 
 
@@ -35,30 +85,39 @@ function clickOutside(e){
 
 /* -------------------- Code ajout de livre ------------------ */
 
-let myLibrary = [];  /* where i store all my books */
+let myLibrary = [];
+let newBook;
 
-
-function Book(title,author,pages, read){
+function Book(title, author, pages, isRead){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read
+    this.isRead = isRead;
 }
 
 
 function addBookToLibrary(){
-    const livre = new Book('FootRue', 'Zidane', 165);
-    myLibrary.push(livre);
-    const livre2 = new Book('Lola', 'Kubrik', 165);
-    myLibrary.push(livre2);
-    const livre3 = new Book('Terminator', 'Anorld', 165);
-    myLibrary.push(livre3);
+    
+   if(checkbox.checked){
+        newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, true);
+   }else{
+        newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, false);
+   }
+
+    myLibrary.push(newBook);
 }
 
-addBookToLibrary();
+/*
+function display(array){
+    for(let i = 0; i < array.length; i++){
+        log(array[i]);
+    }
+} */
 
-for(let i = 0; i < myLibrary.length; i++){
-    log(myLibrary[i]);
+function addBook(e){
+    e.preventDefault();
+    addBookToLibrary();
+    createBook(myLibrary);
 }
 
 
