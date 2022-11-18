ea7b9de8d7b00acf9860f2e0aa9ba61aadaaa06c
Etno Polino <99446834+EtnoPolino@users.noreply.github.com>
2022-11-18 06:49:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,107 @@
-/* -------------- Modal gestion ----------------- */
-const log = console.log;
 const modal = document.querySelector('#modal');
-const addBookModal = document.querySelector('#modalBtn');
-const closeModalBtn = document.querySelector('.closeBtn');
 const formSubmit = document.querySelector('form');
 const bookscontainer = document.querySelector('.bookcontainer');
 const inputTitle = document.querySelector('#title');
 const inputAuthor = document.querySelector('#author');
 const inputPages = document.querySelector('#pages');
 const checkbox = document.querySelector('input[type=checkbox]');
+const main = document.querySelector('main');
 
-
-/*
 
 let myLibrary = [];
-let newBook; */
 
-/*---------------------------------------------------------------------------------------------------------------
-                                                AddEvent listener
------------------------------------------------------------------------------------------------------------------*/
-/*
+main.addEventListener('click', (event) => {
+    if(event.target.classList.contains('button')){
+        openModal();
+    }
 
-addBookModal.addEventListener('click', openModal);
-closeModalBtn.addEventListener('click', closeModal);
-formSubmit.addEventListener('submit', addBook);
+    if(event.target.classList.contains('closeBtn')){
+        closeModal()
+    }
+
+    if(event.target.classList.contains('remove')){
+        const idClicked = event.target.getAttribute('data-id');
+        const bookClickedUI = document.querySelector(`[data-id="${idClicked}"]`);
+        deleteBookUI(bookClickedUI);
+        deleteBookFromlibrary(idClicked);
+    }
+
+    if(event.target.classList.contains('isRead')){
+        const idClicked = event.target.getAttribute('data-id');
+        toogleReadUI(event.target)
+        updateRead(idClicked);
+    }
+});
+
+formSubmit.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value);
+    addBookToLibrary(newBook);
+    UIcreateBook(newBook);
+    formSubmit.reset();
+    closeModal();
+});
+
 window.addEventListener('click', clickOutside);
 
-*/
 
+/*---------------------------------------------------------------- class -----------------------------------------------------------------------*/
 
+class Book{
+    constructor(title, author, pages){
+        this.id = Date.now().toString()
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = checkbox.checked;
+    }
 
-/*---------------------------------------------------------------------------------------------------------------
-                                                functions
------------------------------------------------------------------------------------------------------------------*/
-/*
+    getIsRead(){
+        return this.isRead;
+    }
+
+    updateIsRead(newIsRead){
+        this.isRead = newIsRead;
+    }
+}
+
+/*-------------------------------------------------------------- fonctions ------------------------------------------------------------------------*/
+
 function openModal(){
     modal.style.display = 'block';
     modal.style.transition = 'all 0.5sec ease-in';
 }
 
-function closeModal(){
-    modal.style.display = 'none';
+function addBookToLibrary(book){
+    myLibrary.push(book);
+}
+
+function UIcreateBook(book){
+    let note; 
+
+    if(book.isRead){
+        note = `
+        <div class='note' data-id = ${book.id}>
+            <p>Title : ${book.title}</p>
+            <p>Author : ${book.author}</p>
+            <p>Pages : ${book.pages}</p>
+            <button class='isRead read' data-id = ${book.id}>READ</button>
+            <button class='remove' data-id = ${book.id}>DELETE</button>
+        </div> `;
+    }else{
+        note = `
+        <div class='note' data-id = ${book.id}>
+            <p>Title : ${book.title}</p>
+            <p>Author : ${book.author}</p>
+            <p>Pages : ${book.pages}</p>
+            <button class='isRead notread' data-id = ${book.id}>NOT READ YET</button>
+            <button class='remove' data-id = ${book.id}>DELETE</button>
+        </div> `;
+    }
+
+    const position = 'beforeend';
+    bookscontainer.insertAdjacentHTML(position, note);
 }
 
 function clickOutside(e){
@@ -49,146 +110,37 @@ function clickOutside(e){
     }
 }
 
-function createBook(book){
-    const lastElem = book[book.length - 1];
-     
-    
-    const bookInfo = document.createElement('div');
-    const deleteNote = document.createElement('button');
-    const readOrNot = document.createElement('button');
-    const p_1 = document.createElement('p');
-    const p_2 = document.createElement('p');
-    const p_3 = document.createElement('p');
+function closeModal(){
+    modal.style.display = 'none';
+}
 
-    readOrNot.addEventListener('click', toogleRead);
-    deleteNote.addEventListener('click', deleteBookFromlibrary);
-    
-    p_1.innerText = `Title : ${lastElem.title}`;
-    p_2.innerText = `Author : ${lastElem.author}`;
-    p_3.innerText = `Pages : ${lastElem.pages}`;
+function deleteBookFromlibrary(id){
+    myLibrary = myLibrary.filter((book) => book.id !== id)
+}
 
-    if(lastElem.isRead){
-        readOrNot.innerText = `READ`;
-        readOrNot.classList.add('read');
+function getBookById(id){
+    const myBook = myLibrary.filter((book) => book.id === id);
+    return myBook[0];
+}
+
+function deleteBookUI(book){
+    bookscontainer.removeChild(book);
+}
+
+function toogleReadUI(target){
+    if(target.classList.contains('read')){
+        target.classList.remove('read');
+        target.classList.add('notread');
+        target.textContent = `NOT READ YET`;
     }else{
-        readOrNot.innerText = `NOT READ YET`;
-        readOrNot.classList.add('notread');
-    }
-
-    deleteNote.innerText = `DELETE`;
-    bookInfo.classList.add('note');
-    deleteNote.classList.add('remove');
-    bookInfo.appendChild(p_1);
-    bookInfo.appendChild(p_2);
-    bookInfo.appendChild(p_3);
-    bookInfo.appendChild(readOrNot);
-    bookInfo.appendChild(deleteNote);
-    bookscontainer.appendChild(bookInfo);
-
-    function deleteBookFromlibrary(){
-        bookInfo.parentNode.removeChild(bookInfo);
-        myLibrary = myLibrary.filter((b) => b.title !== `${lastElem.title}`);
-    }
-
-   closeModal();
-   formSubmit.reset();
-}
-
-*/
-
-/* -------------------- Code ajout de livre ------------------ */
-
-/*
-
-function Book(title, author, pages, isRead){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-}
-
-function addBookToLibrary(){ 
-   if(checkbox.checked){
-        newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, true);
-   }else{
-        newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, false);
-   }
-    myLibrary.push(newBook);
-}
-
-function addBook(e){
-    e.preventDefault();
-    addBookToLibrary();
-    createBook(myLibrary);
-}
-
-function toogleRead(){
-    if(this.classList.contains('read')){
-        this.classList.remove('read');
-        this.classList.add('notread');
-        this.innerText = `NOT READ YET`;
-    }else{
-        this.classList.remove('notread');
-        this.classList.add('read');
-        this.innerText = `READ`;
+        target.classList.remove('notread');
+        target.classList.add('read');
+        target.textContent = `READ`;
     }
 }
 
-*/
-
-/*--------------------------------------------------Refactor all the code-------------------------------------------------*/
-
-const main = document.querySelector('main');
-let myLibrary = [];
-
-main.addEventListener('click', (event) => {
-    if(event.target.classList.contains('button')){
-        openModal();
-    }
-})
-
-formSubmit.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, checkbox.value);
-    addBookToLibrary(newBook);
-    UIcreateBook(newBook);
-});
-
-
-
-
-class Book{
-    constructor(title, author, pages){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.isRead = false;
-    }
-}
-
-
-function openModal(){
-    modal.style.display = 'block';
-    modal.style.transition = 'all 0.5sec ease-in';
-}
-
-
-function addBookToLibrary(book){
-    myLibrary.push(book);
-}
-
-function UIcreateBook(book){
-    const note = `
-        <div class='note'>
-            <p>Title : ${book.title}</p>
-            <p>Author : ${book.author}</p>
-            <p>Pages : ${book.pages}</p>
-            <button class='read'>READ</button>
-            <button class='remove'>DELETE</button>
-        </div>
-    `;
-
-    const position = 'beforeend';
-    bookscontainer.insertAdjacentHTML(position, note);
+function updateRead(id){
+    const book = getBookById(id);
+    const bookIsRead = book.getIsRead();
+    book.updateIsRead(!bookIsRead)
 }
